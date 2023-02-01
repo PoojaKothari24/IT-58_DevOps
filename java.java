43dfedaa109f1397eb4c44b709116717a1d13674
PoojaKothari24 <95275326+PoojaKothari24@users.noreply.github.com>
2023-02-01 10:04:57 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,8 @@
+public class LengthExample{  
+public static void main(String args[]){  
+String s1="Pooja Kothari";  
+String s2="Java Code to find length of str";  
+System.out.println("string length is: "+s1.length());  
+System.out.println("string length is: "+s2.length());  
+}}  
+
